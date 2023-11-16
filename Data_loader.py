297a16a35ad17a0d94ce4bfef5072b1cb3b0a576
Rakshith Vaishnavi Dogra <rakshithdogra@gmail.com>
2023-11-17 00:49:03 +0530

    some changes were made here and there

--- Data_loader.py
+++ Data_loader.py
@@ -11,3 +11,6 @@ train_data = pd.read_csv("mushroom_trn_data.csv")
 def dummy_enc(data_df):
     enc = pd.get_dummies(data_df, columns=['cap-shape', 'cap-surface', 'cap-color', 'bruises', 'odor', 'gill-attachment', 'gill-spacing', 'gill-size', 'gill-color', 'stalk-shape', 'stalk-root', 'stalk-surface-above-ring', 'stalk-surface-below-ring', 'stalk-color-above-ring', 'stalk-color-below-ring', 'veil-type', 'veil-color', 'ring-number', 'ring-type', 'spore-print-color', 'population', 'habitat'], dtype = int)
     return enc
+def dummy_enc_y(data_df_y):
+    enc = pd.get_dummies(data_df_y)
+    return enc
